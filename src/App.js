d03d2f691e66183b5742e18d88da6af574015019
Cristina Fernandez Bornay <cristinafernandezbornay@outlook.com>
2021-01-09 14:08:12 +0100

    feat(front): clean code
    feat(App.js): working post petition to the back

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
 
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
+  const handlePostQuery = async () => {
+    const data = {
+      rawEquation: '-98 * X^0 -2.6 * X^1 -3.1 * X^2 = -4 * x^0 -23 * X^1 -3.1 * X^2 ',
+    }
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': 'Content-Type',
+      },
+      mode: 'cors',
+      body: JSON.stringify(data)
+    }
+    await fetch('http://localhost:5000/api/', requestOptions)
+    .then( async(responsePetition)  => {
+      const response = await responsePetition.json()
+      setCurrentTime(response.time)
+      console.log(response)
+    })
+    .catch( error => {
+        console.log(error);
     });
-  }, []);
+}
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
 
+        <button
+          onClick={handlePostQuery}
+        >Hello lol</button>
+        <br/>
         ... no changes in this part ...
-
         <p>The current time is {currentTime}.</p>
       </header>
     </div>
