e2f74a2683a494bea77ea1e64e453e36295ca890
cristina <cristina@42network.org>
2021-01-23 14:44:22 +0100

    fix(api): check error parser

--- api/src/parser.py
+++ api/src/parser.py
@@ -3,12 +3,14 @@ from src.classes.subequation import Subequation
 
 def checkCorrectCharacters(input):
     admitedCharacters = '0123456789xX*^= '
-    for c in admitedCharacters:
-        if input.count(c) == 0:
+    for c in input:
+        if admitedCharacters.count(c) == 0:
             return 'ERROR'
 
 def check_errors(input):  
-    if input.count('=') != 1 or checkCorrectCharacters(input) == 'ERROR':
+    if input.count('=') != 1:
+        return 'ERROR'
+    if checkCorrectCharacters(input) == 'ERROR':
         return 'ERROR'
 
 # PARSING
@@ -16,7 +18,8 @@ def find_subequation(input, index):
     return input.split('=')[index].strip().replace('X', 'x').replace(' ', '')
 
 def parse(input):
-    check_errors(input)
+    if check_errors(input) == 'ERROR':
+        return 'ERROR'
     subequation1 = Subequation(find_subequation(input,0))
     subequation2 = Subequation(find_subequation(input,1))
     if subequation1.degree_higher_than_2 == 1 or subequation2.degree_higher_than_2 == 1:
