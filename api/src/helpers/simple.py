6a937fa174d923d383ca40fa5450efb7fb9cd266
cristina <cristina@42network.org>
2021-01-26 15:16:09 +0100

    feat(api): error message

--- api/src/helpers/simple.py
+++ api/src/helpers/simple.py
@@ -1,7 +1,7 @@
 ## SOLVE LINEAR EQUATION --> DEGREE = 1 : A = 0 || B = 0
 def simple_function(equation):
     if equation.c != 0:
-        equation.info = 'Beloved human, your beloved number cannot be zero.'
+        equation.info = 'Beloved human, there is no solution for your beloved equation.'
     else:
         equation.info = 'Every real number is a possible solution of this equation.'
     return equation
